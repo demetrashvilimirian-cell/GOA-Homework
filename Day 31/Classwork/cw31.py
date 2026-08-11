250ef14f99434e8babf8a81eb1c6e4cf138738e6
demetrashvilimirian-cell <demetrashvilimirian@gmail.com>
2026-08-11 19:02:17 +0400

    second commit

---
+++ Day 31/Classwork/cw31.py
@@ -0,0 +1,36 @@
+# 1
+# დღეს ვისწავლეთ 1 ფუნქცია და 3 მეთოდი, len(),.append(),.insert(),.pop()
+
+# მაგალითს მოვიყვან 4 ივეზე
+
+# len()
+listt = ["Mirian", "Demetrashvili"]
+print(len(listt))
+
+# .append()
+listtt = ["Demetrashvili", "Mirian"]
+listtt.append("12 Years Old")
+print(listtt)
+
+# .insert()
+listttt = ["Miriani", "Wlisaa"]
+listttt.insert(1,"12")
+print(listttt)
+
+# .pop()
+listtttt = ["Miriani", "12 Wlis","Ar Aris","Aris"]
+listtttt.pop(2)
+
+# 2
+listttttt = ["Goa","Oriented","Academy"]
+print(len(listttttt))
+
+# 3
+colors = ["Red", "Green", "Blue", "Yellow", "Purple"]
+colors.pop(4)
+print(colors)
+
+# 4
+animals = ["Dog", "Cat", "Elephant", "Lion"]
+animals.insert(2,"Monkey")
+print(animals)
