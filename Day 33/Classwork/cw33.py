e75493873020c847bee341a8ebdd8576e86bb5c2
demetrashvilimirian-cell <demetrashvilimirian@gmail.com>
2026-08-19 16:42:06 +0400

    second commit

---
+++ Day 33/Classwork/cw33.py
@@ -0,0 +1,23 @@
+# 1
+
+def greet(name):
+    print("გამარჯობა,", name)
+greet("მირიან")
+
+# 2
+
+def double(number):
+    print(number * 10)
+double(5)
+
+# 3
+
+def square(number):
+    print(number * number)
+square(8)
+
+# 4
+
+def sum(a, b):
+    return a + b
+sum(5, 7)
